63d2fca1c24dabacf0f8b053cf59bb07f5abb987
Vi Quynh <vi.quynh@hotmail.com>
2017-08-28 15:34:30 +0700

    add file for demo git on Intellij

---
+++ src/main/java/com/higgsup/bizwebcrawler/object/SayHello.java
@@ -0,0 +1,11 @@
+package com.higgsup.bizwebcrawler.object;
+
+/**
+ * Created by viquynh
+ */
+public class SayHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hi ^^");
+    }
+}
