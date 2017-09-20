8887a9edc7bf6edb331a3a5ea681aabbc482903e
Vi Quynh <vi.quynh@hotmail.com>
2017-09-20 15:16:11 +0700

    --add try catch in ConnectDB class

--- src/main/java/com/higgsup/bizwebcrawler/controller/managedatabase/ConnectDB.java
+++ src/main/java/com/higgsup/bizwebcrawler/controller/managedatabase/ConnectDB.java
@@ -8,14 +8,18 @@ import java.util.logging.Logger;
  * @author viquynh
  */
 public class ConnectDB {
-    private static final Logger logger = Logger.getLogger("ConnectDB");
+    private static final Logger logger = Logger.getLogger(ConnectDB.class.getName());
     protected static Connection con;
     protected String query;
     protected PreparedStatement ps;
     protected ResultSet rs;
     public Connection startConnect() throws ClassNotFoundException {
+        Connection con = null;
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        }catch (ClassNotFoundException e){
+            logger.log(Level.SEVERE, e.getMessage());
+        }try{
             con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=TestWebCrawler;Username=sa;Password =123456");
         } catch (SQLException e) {
             logger.log(Level.SEVERE, e.getMessage());
